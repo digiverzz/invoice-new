f3a954eb8c0b46e84adc1ade19733a254e3f60e6
digiverzz <digiverzkaartech@gmail.com>
2023-05-04 18:42:15 +0530

    DOcker error cleared

--- invoice-frontend/src/utils/request.js
+++ invoice-frontend/src/utils/request.js
@@ -1,4 +1,4 @@
-// const URI = "http://172.174.180.163:5000/";
-const URI = "http://127.0.0.1:5000/";
+const URI = "http://172.174.180.163:5000/";
+// const URI = "http://127.0.0.1:5000/";
 
 export default URI;
